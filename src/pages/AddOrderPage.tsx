76c4848bc323b940e84030cc520cc3b11e377b20
said alhajri <facegoogl@gmail.com>
2022-11-02 12:03:38 +0400

    order page updated fully

--- src/pages/AddOrderPage.tsx
+++ src/pages/AddOrderPage.tsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonFab, IonFabButton,
-   IonFooter,
-   IonIcon, IonInput, IonItem, IonLabel, IonList, IonMenu, IonModal, IonPage,
-     IonSearchbar,
-     IonSelect,
-     IonSelectOption,
-     IonTitle,
-     useIonAlert} from '@ionic/react';
+import {
+  IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCheckbox, IonCol, IonContent, IonDatetime, IonDatetimeButton, IonFab, IonFabButton,
+  IonFooter,
+  IonGrid,
+  IonIcon, IonImg, IonInput, IonItem, IonLabel, IonList, IonMenu, IonModal, IonPage,
+  IonRow,
+  IonSearchbar,
+  IonSegment,
+  IonSegmentButton,
+  IonSelect,
+  IonSelectOption,
+  IonSpinner,
+  IonTitle,
+  IonVirtualScroll,
+  SelectChangeEventDetail,
+  useIonAlert
+} from '@ionic/react';
 import './Home.css';
-import { caretForwardOutline, closeSharp, duplicateOutline, location as locationIcon } from 'ionicons/icons';
+import { arrowDown, arrowForwardOutline, caretForwardOutline, closeSharp, duplicateOutline, fastFood, location as locationIcon } from 'ionicons/icons';
 import { useHistory } from "react-router-dom";
 import { useGlobals } from '../providers/globalsProvider';
 import * as L from 'leaflet';
@@ -16,142 +25,265 @@ import 'leaflet/dist/leaflet.css';
 import { getUserInfoPlaceHolder } from '../providers/firebaseMain';
 import { Device } from '@capacitor/device';
 import { citiesList } from '../components/utlis/citiesUtlis';
+import { Url } from 'url';
+import { addDoc, collection, doc, FieldValue, serverTimestamp } from 'firebase/firestore';
+import { db } from '../App';
+import { getAuth } from 'firebase/auth';
 
-const optionsPlaceholder:any=[]
-for(let v of Array(4)){
-  optionsPlaceholder.push({value:'oman',title:'muscat',subTitle:"oman"})
+const optionsPlaceholder: any = []
+for (let v of Array(4)) {
+  optionsPlaceholder.push({ value: 'oman', title: 'muscat', subTitle: "oman" })
 }
-type locationOption ={value:Geolocation|any,title:string,subTitle?:string}
-type Geolocation={latlng:LatLng,city?:string,state?:string}
-type LatLng = {lat:string,lng:string}
-const AddOrderPage= () => {
-  
-  const {user,profile}= useGlobals()
+type locationOption = { value: Geolocation | any, title: string, subTitle?: string }
+type Geolocation = { latlng: LatLng, city?: string, state?: string }
+type LatLng = { lat: string, lng: string }
+type OrderType = { name: string, icon: any, value: string }
+const catagories: OrderType[] = [
+  { name: 'Small Objects', icon: require('../assets/smallObjectsIcon.png'), value: 'SmallObjects' }
+  , { name: 'Food & Drinks', icon: require('../assets/take-away.png'), value: 'Food' }
+  , { name: 'People Transport', icon: require('../assets/peopleTransIcon.png'), value: 'PeopleTrans' }
+  , { name: 'Animal Transport', icon: require('../assets/animalsTrans.png'), value: 'AnimalTrans' }
+  , { name: 'Big Objects', icon: require('../assets/interior-design.png'), value: 'BigObjects' }]
+
+
+
+const AddOrderPage = () => {
+
+  const { user, profile } = useGlobals()
   const history = useHistory()
-  const [addOrder,setAddOrder] = useState(false)
-  const [pickUpLocation,setPickUpLocation] = useState<Geolocation>()
-  const [dropLocation,setDropLocation] = useState<Geolocation>()
+  const [orderCatagory, setOrderCatagory] = useState<string>('')
+  const [pickUpLocation, setPickUpLocation] = useState<Geolocation>()
+  const [dropLocation, setDropLocation] = useState<Geolocation>()
 
-  const [pickUpSearchValue,setPickUpSearchValue] = useState<string>()
-  const [dropSearchValue,setDropSearchValue] = useState<string>()
+  const [timeSegment, setTimeSegment] = useState<string>("anytimeSoon")
 
-  const [focusedPicker,setFocusedPicker] = useState<"pickUp"|"drop">()
+  const [pickUpSearchValue, setPickUpSearchValue] = useState<string>()
+  const [dropSearchValue, setDropSearchValue] = useState<string>()
 
-  const [fcmToken,setFcmToken] = useState<any>(null)
-  const [_profile,_setProfile] = useState<any>(profile?profile:getUserInfoPlaceHolder())
-  const [options,setOptions] = useState<locationOption[]>()
+  const [focusedPicker, setFocusedPicker] = useState<"pickUp" | "drop">()
+  const [isLocationsConfirmed, setLocationsConfirmed] = useState<boolean>(false)
+
+  const [fcmToken, setFcmToken] = useState<any>(null)
+  const [_profile, _setProfile] = useState<any>(profile ? profile : getUserInfoPlaceHolder())
+  const [options, setOptions] = useState<locationOption[]>()
+  const [isSubmitingOrder, setSubmitingOrder] = useState(false)
+  const [isUrgent, setUrgent] = useState(false)
+  const [OrderDate, setOrderDate] = useState<Date | any>(new Date().toDateString())
+
+
   useEffect(() => {
-    if(!!profile){
+    if (!!profile) {
       _setProfile(profile)
     }
   }, [profile]);
 
   function onOptionPick(v: locationOption): void {
-    console.log('on option pick')
-    focusedPicker==='pickUp'? setPickUpLocation(v.value||pesuedoLocation(v)):setDropLocation(v.value||pesuedoLocation(v))
-    console.log('focused  :>> ', focusedPicker );
-    focusedPicker==='pickUp'? 
-    setPickUpSearchValue(v.title||pickUpSearchValue)
-    :setDropSearchValue(v.title ||dropSearchValue)
-    console.log('v.value :>> ', v.title);
+    focusedPicker === 'pickUp' ? setPickUpLocation(v.value || pesuedoLocation(v)) : setDropLocation(v.value || pesuedoLocation(v))
+    focusedPicker === 'pickUp' ?
+      setPickUpSearchValue(v.title || pickUpSearchValue)
+      : setDropSearchValue(v.title || dropSearchValue)
+    setOptions(undefined)
   }
-  function onAddOrder(){
+  function onAddOrder() {
     // presentAlert({message:'hello',buttons:["submit"]})
   }
-  function onPickerFocused(v:"drop"|"pickUp"){
+  function onPickerFocused(v: "drop" | "pickUp") {
     setFocusedPicker(v)
     console.log('onset focused :>> ', v);
   }
-  function pesuedoLocation(v:locationOption|any):Geolocation {
-    return {latlng:{lat:'15.55555',lng:'51,00000'}}
+  function pesuedoLocation(v: locationOption | any): Geolocation {
+    return { latlng: { lat: '15.55555', lng: '51,00000' } }
   }
-  function updateOptions(text:string) {
- 
-    let op: locationOption[]=[]
-    const c = citiesList.sort((a,b)=>{return b.indexOf(text)-a.indexOf(text)}).slice(0,4)
-    c.forEach((v)=>op.push({value:v,title:v,subTitle:v}))
+  function updateOptions(text: string) {
+
+    let op: locationOption[] = []
+    const c = citiesList.sort((a, b) => { return b.indexOf(text) - a.indexOf(text) }).slice(0, 4)
+    c.forEach((v) => op.push({ value: v, title: v, subTitle: v }))
     setOptions(op)
-    
+
   }
   const [presentAlert] = useIonAlert()
 
-  
- function isLocationsSet(){
-  return pickUpLocation && dropLocation
- }
-    return (
+
+  function isLocationsSet() {
+    return pickUpLocation && dropLocation
+  }
+  const onSelect = (e: CustomEvent) => {
+    console.log('event of select :>> ', e.detail.value);
+  }
+  const onSubmitOrder = async () => {
+    if (!user) { presentAlert({ message: 'please Sign In First', animated: true, }) }
+    setSubmitingOrder(true)
+    const newO = { from: pickUpLocation, to: dropLocation, uid: getAuth().currentUser?.uid, time: serverTimestamp(), type: orderCatagory }
+    try {
+      await addDoc(collection(db, 'orders'), newO).finally(() => setSubmitingOrder(false))
+      console.log('oreder submited')
+    } catch (error) {
+      console.log('error submitiing order :>> ', error);
+    }
+  }
+  const Step2 = <IonPage >
+    <IonFab style={{ left: '10px', top: '10px' }}>
+      <IonFabButton color={'light'} onClick={() => setLocationsConfirmed(false)}>
+        <IonIcon color={'primary'} icon={closeSharp} />
+      </IonFabButton>
+    </IonFab>
+    <IonContent style={{ top: '60px' }}>
+      <IonCard style={{ display: 'flex', justifyContent: 'center', justifyItems: 'space-between' }} >
+        {/* pick up point */}
+        <div>
+          <IonLabel
+            position={'floating'}>Pick up point</IonLabel>
+          <IonTitle
+            onClick={() => setLocationsConfirmed(false)}>
+            {pickUpSearchValue}
+          </IonTitle>
+        </div>
+        {/* drop point */}
+        <IonIcon style={{ verticalAlign: 'middle', padding: '4px' }} size={'large'} icon={arrowForwardOutline}></IonIcon>
+        <div>
+          <IonLabel position={'floating'}>Drop point</IonLabel>
+          <IonTitle
+            onClick={() => setLocationsConfirmed(false)}>
+            {dropSearchValue}
+          </IonTitle>
+        </div>
+      </IonCard>
+      <div style={{ display: 'flex', justifyItems: 'space-between', justifyContent: 'center', alignItems: 'space-evenly' }}>
+        {catagories && catagories.map((value, index, array) => {
+          return <div
+            onClick={() => setOrderCatagory(value.value)}
+            key={index}
+            style={{
+              margin: '5px', width: '60px',
+              height: 'auto',
+              borderRadius: '10px',
+              border: '5px',
+              backgroundColor: orderCatagory === value.value ?
+                "var(--ion-color-primary)" : "var(--ion-color-light)"
+            }}>
+
+            <IonImg style={{ flex: 1, }} src={value.icon} />
+            <IonLabel
+              style={{ fontSize: '0.7em' }}
+            >{value.name}</IonLabel>
+          </div>
+        })}
+      </div>
+
+      <div>
+        <IonItem>
+          <IonLabel >Is it Urgent? </IonLabel>
+          <IonCheckbox placeholder={'Is Order Urgent?'} value={isUrgent} onIonChange={() => { setUrgent(!isUrgent) }}>
+          </IonCheckbox>
+        </IonItem>
+        <IonItem>
+          <IonLabel>Time prefered for delivery</IonLabel>
+          <IonDatetimeButton  datetime="datetime"></IonDatetimeButton>
+
+          <IonModal keepContentsMounted={true}>
+            <IonDatetime onIonChange={(e) => { setOrderDate(e.detail.value); console.log(e.detail.value) }} id="datetime"></IonDatetime>
+          </IonModal>
+        </IonItem>
+        {/* <IonDatetime presentation={'date'} 
+        onIonChange={(e) => { setOrderDate(e.detail.value); console.log(e.detail.value) }}
+         placeholder={'Time for Delivery'}></IonDatetime> */}
+      </div>
+
+    </IonContent>
+
+
+    <IonFooter style={{ display: "flex", justifyContent: 'center' }}>
+      <IonButton shape={'round'}
+        disabled={!orderCatagory || !isLocationsConfirmed||!OrderDate||isSubmitingOrder}
+        onClick={() => {
+          onSubmitOrder()
+        }}   >
+          {isSubmitingOrder &&<IonSpinner ></IonSpinner>}
+        Submit Order 👍
+      </IonButton>
+    </IonFooter>
+
+
+
+
+    {/* {"dInfo: "+JSON.stringify( dInfo)}{"state: "+state} */}
+
+  </IonPage>
+
+  if (isLocationsConfirmed) {
+    return Step2
+  }
+
+  return (
     <IonPage >
+      <IonFab style={{ left: '10px', top: '10px' }}>
+        <IonFabButton color={'light'} onClick={() => history.goBack()}>
+          <IonIcon color={'primary'} icon={closeSharp} />
+        </IonFabButton>
+      </IonFab>
+      <IonContent style={{ top: '60px' }}>
+        <IonCard
+        // style={{top:'60px'}}
+        >
+          <IonCardContent>
+            {/* pick up point */}
+            <IonItem>
+              <IonLabel
+                position={'floating'}>Pick up point</IonLabel>
+              <IonInput
+                // onIonBlur={()=>setOptions(undefined)}
+                value={pickUpSearchValue}
+                onIonChange={(e) => { updateOptions(e.detail.value || ""); setPickUpSearchValue(e.detail.value || "") }}
+                onClick={() => onPickerFocused("pickUp")}></IonInput>
+            </IonItem>
+            {/* drop point */}
+            <IonItem>
+              <IonLabel position={'floating'}>Drop point</IonLabel>
+              <IonInput
+                // onIonBlur={()=>setOptions(undefined)}
+                value={dropSearchValue}
+                onIonChange={(e) => { updateOptions(e.detail.value || ""); setDropSearchValue(e.detail.value || "") }}
+                onClick={() => onPickerFocused("drop")}></IonInput>
+            </IonItem>
+          </IonCardContent>
+        </IonCard>
 
-      
-      
-      
-      <IonFab style={{left: '10px',top:'10px'}}>
-              <IonFabButton color={'light'}  onClick={()=>history.goBack()}>
-                  <IonIcon color={'primary'} icon={closeSharp} />
-              </IonFabButton>
-            </IonFab>
-                <IonContent style={{top: '60px'}}>
-                  <IonCard 
-                  // style={{top:'60px'}}
-                  >
-                    <IonCardContent>
-                      {/* pick up point */}
-                      <IonItem>
-                        <IonLabel
-                        position={'floating'}>Pick up point</IonLabel>
-                        <IonInput
-                        onIonBlur={()=>setOptions(undefined)}
-                        value={pickUpSearchValue}
-                        onIonChange={(e)=>{updateOptions(e.detail.value||"");setPickUpSearchValue(e.detail.value||"")}}
-                         onClick={()=>onPickerFocused("pickUp")}></IonInput>
-                      </IonItem>
-                      {/* drop point */}
-                      <IonItem>
-                        <IonLabel position={'floating'}>Drop point</IonLabel>
-                        <IonInput
-                        onIonBlur={()=>setOptions(undefined)}
-                        value={dropSearchValue}
-                        onIonChange={(e)=>{updateOptions(e.detail.value||"");setDropSearchValue(e.detail.value||"")}} 
-                        onClick={()=>onPickerFocused("drop")}></IonInput>
-                      </IonItem>
-                    </IonCardContent>
-                  </IonCard>
+        <IonList inset={true}  >
+          <IonItem key={'head'}>
+            <IonIcon color={'primary'} icon={locationIcon}></IonIcon>
+            <IonCardTitle>{'use map for location'}</IonCardTitle>
+          </IonItem>
+          {options && options.map((v, i) => {
+            return <IonItem key={i} onClick={() => { onOptionPick(v) }}>
+              <IonIcon size={'small'} color={'secondary'} icon={caretForwardOutline}></IonIcon>
 
-                  <IonList inset={true}  >
-                  <IonItem key={'head'}>
-                          <IonIcon color={'primary'} icon={locationIcon}></IonIcon>
-                          <IonCardTitle>{'use map for location'}</IonCardTitle>
-                        </IonItem>
-                    {options && options.map((v,i)=>{
-                        return<IonItem key={i} onClick={()=>{onOptionPick(v);console.log('message')}}>
-                          <IonIcon size={'small'} color={'secondary'} icon={caretForwardOutline}></IonIcon>
-                          
-                          <IonCol>
-                            <IonLabel>{v.title}</IonLabel>
-                            <IonCardSubtitle >{v.subTitle ||''}</IonCardSubtitle>
-                          </IonCol>
-                          
-                        </IonItem>
-                    })}
+              <div>
+                <IonLabel>{v.title}</IonLabel>
+                <IonCardSubtitle  >{v.subTitle || ''}</IonCardSubtitle>
+              </div>
 
-                  </IonList>
+            </IonItem>
+          })}
 
-                </IonContent>
-               
-               
-               <IonFooter style={{display:"flex",justifyContent: 'center'}}>
-               <IonButton shape={'round'}
-                  disabled={!isLocationsSet()}
-                  onClick={()=>onAddOrder()}   >
-                    Set Locations
-                  </IonButton>
-                </IonFooter>   
-              
-              
-              
+        </IonList>
 
-              {/* {"dInfo: "+JSON.stringify( dInfo)}{"state: "+state} */}
-           
+      </IonContent>
+
+
+      <IonFooter style={{ display: "flex", justifyContent: 'center' }}>
+        <IonButton shape={'round'}
+          disabled={!isLocationsSet()}
+          onClick={() => setLocationsConfirmed(true)}   >
+          Set Locations
+        </IonButton>
+      </IonFooter>
+
+
+
+
+      {/* {"dInfo: "+JSON.stringify( dInfo)}{"state: "+state} */}
+
     </IonPage>
   );
 };
--- src/pages/ApplicationsPage.tsx
+++ src/pages/ApplicationsPage.tsx
@@ -1,45 +1,45 @@
 import React, { FC, useEffect, useReducer, useState } from 'react';
-import { IonContent, IonPage, IonTitle, IonToolbar,IonButtons, IonInput, IonLabel, IonItem, IonAccordionGroup, IonAccordion, IonList, IonSpinner, IonBackButton, IonSlides, IonSlide, IonCard, IonCardTitle, IonSegment, IonSegmentButton, IonChip, IonIcon } from '@ionic/react';
+import { IonContent, IonPage, IonTitle, IonToolbar, IonButtons, IonInput, IonLabel, IonItem, IonAccordionGroup, IonAccordion, IonList, IonSpinner, IonBackButton, IonSlides, IonSlide, IonCard, IonCardTitle, IonSegment, IonSegmentButton, IonChip, IonIcon } from '@ionic/react';
 import { useGlobals } from '../providers/globalsProvider';
 import { collection, doc, DocumentData, DocumentSnapshot, FieldValue, getFirestore, onSnapshot, orderBy, query, where } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import "./Profile.css"
-import {  useHistory, useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 import { TT } from '../components/utlis/tt';
 import { db } from '../App';
 import { ApplicationProps, getUserInfoPlaceHolder, makeApplicationPropsFromDoc, makeUSerInfoFromDoc, userInfo } from '../providers/firebaseMain';
 import { thumbsDown, thumbsUp } from 'ionicons/icons';
 
 const ApplicationsPage: React.FC = () => {
-    const {user,profile} = useGlobals()
-    const [loading,setLoading]=useState(true)
-    const [data,setData] = useState<DocumentSnapshot<DocumentData>[]>([])
-    const [segment,setsegment] = useState<"byUser"|"forUser">("byUser")
+  const { user, profile } = useGlobals()
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState<DocumentSnapshot<DocumentData>[]>([])
+  const [segment, setsegment] = useState<"byUser" | "forUser">("byUser")
 
-    const auth= getAuth()
-    const id:any = useParams()
-    const history =useHistory()
-    
-    useEffect(()=>{
-      if(!id.id){
-        alert("uncorrect ref")
-        return
-      }
-      var d:any = []
-      const v = onSnapshot(query(collection(getFirestore(),"ordersApplications/"+id.id+"/col")),(docs)=>{
-        docs.docs.forEach((value)=>{
-          d.push(value.data())
-        })
-        setData(d)
+  const auth = getAuth()
+  const id: any = useParams()
+  const history = useHistory()
 
+  useEffect(() => {
+    if (!id.id) {
+      alert("uncorrect ref")
+      return
+    }
+    var d: any = []
+    const v = onSnapshot(query(collection(getFirestore(), "ordersApplications/" + id.id + "/col")), (docs) => {
+      docs.docs.forEach((value) => {
+        d.push(value.data())
       })
-      return()=>v()
-  },[]);
-   
-    
-    return (
+      setData(d)
+
+    })
+    return () => v()
+  }, []);
+
+
+  return (
     <IonPage >
-      
+
       <IonToolbar color="secondary">
         <IonButtons slot="start">
           <IonBackButton defaultHref="/home" />
@@ -52,12 +52,12 @@ const ApplicationsPage: React.FC = () => {
         <IonSegmentButton></IonSegmentButton>
       </IonSegment>
       <IonContent>
-            {data &&  data.map((value) => {
-            return<ApplicationCard docsnap={value}>
-              
-            </ApplicationCard>
-          })}
-        
+        {data && data.map((value) => {
+          return <ApplicationCard docsnap={value}>
+
+          </ApplicationCard>
+        })}
+
       </IonContent>
     </IonPage>
   );
@@ -66,36 +66,36 @@ const ApplicationsPage: React.FC = () => {
 export default ApplicationsPage;
 
 
-export const ApplicationCard:React.FC<{docsnap:DocumentSnapshot<DocumentData>}> = ({docsnap})=>{
-  const [user,setUser]= useState<userInfo>(getUserInfoPlaceHolder())
-  const [data,setData]= useState<ApplicationProps>(makeApplicationPropsFromDoc(docsnap))
-  
-  useEffect(()=>{
+export const ApplicationCard: React.FC<{ docsnap: DocumentSnapshot<DocumentData> }> = ({ docsnap }) => {
+  const [user, setUser] = useState<userInfo>(getUserInfoPlaceHolder())
+  const [data, setData] = useState<ApplicationProps>(makeApplicationPropsFromDoc(docsnap))
 
-    const unsub = onSnapshot(doc(db,"users",data.byUser),(snap)=>{
+  useEffect(() => {
+
+    const unsub = onSnapshot(doc(db, "users", data.byUser), (snap) => {
       setUser(makeUSerInfoFromDoc(snap))
     })
-    return()=>{unsub()}
-  },[])
+    return () => { unsub() }
+  }, [])
 
-  return<IonCard>
+  return <IonCard>
     <IonItem>
       <IonLabel>Name :</IonLabel>
       <IonLabel>{user.name}</IonLabel>
     </IonItem>
     <IonItem>
       <IonLabel>time :</IonLabel>
-      {new Date(data.timeSend.seconds*1000).toLocaleString()}
+      {new Date(data.timeSend.seconds * 1000).toLocaleString()}
     </IonItem>
     <IonItem>
       <IonLabel>isAccepted :</IonLabel>
-      <IonLabel>{data.isAccepted?"Yes":"No"}</IonLabel>
+      <IonLabel>{data.isAccepted ? "Yes" : "No"}</IonLabel>
     </IonItem>
     <IonItem dir='ltr'>
-      Done: {data.isDone?"YES ":"NO "}
-        <IonIcon icon={data.isDone?thumbsUp:thumbsDown}></IonIcon>
-        On Date {new Date(data.timeDone.seconds*1000).toLocaleString()}
-      
+      Done: {data.isDone ? "YES " : "NO "}
+      <IonIcon icon={data.isDone ? thumbsUp : thumbsDown}></IonIcon>
+      On Date {new Date(data.timeDone.seconds * 1000).toLocaleString()}
+
     </IonItem>
     <IonItem>
       <IonLabel>for user :</IonLabel>
