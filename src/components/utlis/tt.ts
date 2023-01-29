86e6f278bcb9391006d160d086a4826cc8526680
said alhajri <facegoogl@gmail.com>
2023-01-29 22:34:01 +0400

    transation system improved to log the updated translation json

--- src/components/utlis/tt.ts
+++ src/components/utlis/tt.ts
@@ -1,23 +1,45 @@
-const ttData = require("./transulatedText.json")
-// transulated text
-export function TT(text:string,language:string="en"){
-    var t = ttData[text]
-    var l = t?ttData[text][language]:false
+import { getLang } from "../../App";
+// import fs from "fs";
 
-    if(t){
-        if(l){
-            return l
-        }else{
-            // ttData.writeFile(t,{})
-        }
-    }else{
-        var d = ttData
-        d[text]={en:text}
-        d= JSON.stringify(d)
-        // ttData.writeFile("transulatedText.json",d,()=>{
-        //     console.log("done")
-        // })
+const ttData = require("../../assets/transulatedText.json");
+
+// transulated text
+export function TT(text: string, language?: string) {
+  language || (language = getLang());
+  var key = ttData[text];
+  var langKey = key ? ttData[text][language]?ttData[text][language]:false : false;
+
+  if (key) {
+    if (langKey) {
+      return langKey;
+    } else {
+      let newdata = ttData;
+      newdata[text][language] = text;
+      newdata = JSON.stringify(newdata);
+      update(newdata);
     }
-    return text
+  } else {
+    let newdata = ttData;
+    newdata[text] =  {en:text} ;
+    newdata = JSON.stringify(newdata);
+    update(newdata);
+  }
+  return text;
 }
 // TT("hello")
+function update(newData: any) {
+    console.log('new translation file :>> ', newData);
+// newData
+//     "../../assets/transulatedText.json",
+//     newData,
+//     "utf8",
+//     function (err: any) {
+//       if (err) {
+//         console.log("An error occured while writing JSON Object to File.");
+//         return console.log(err);
+//       }
+
+//       console.log("translation JSON file has been saved.");
+//     }
+//   );
+}
