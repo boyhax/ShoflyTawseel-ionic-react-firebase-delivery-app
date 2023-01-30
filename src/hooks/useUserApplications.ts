58d6c800a09c044ea69d95cbc9d33211ffb29972
said alhajri <facegoogl@gmail.com>
2023-01-30 22:56:36 +0400

    now using stores for list of user

--- src/hooks/useUserApplications.ts
+++ src/hooks/useUserApplications.ts
@@ -2,16 +2,16 @@ import { getAuth } from 'firebase/auth';
 import { DocumentData, QuerySnapshot } from 'firebase/firestore';
 import * as React from 'react';
 import { useState } from 'react';
-import { subscripeUserApplications, } from '../providers/firebaseMain';
+import { subscripeUserApplications, userApplicationsStore, } from '../providers/firebaseMain';
 
 
 
 const useUserApplications = () => {
-    const [userApplications, setUserApplications] = React.useState<QuerySnapshot<DocumentData>>()
+    // const [userApplications, setUserApplications] = React.useState<QuerySnapshot<DocumentData>>()
     const [loading, setLoading] = React.useState<boolean>(true)
     const [mounted, setMounted] = useState(true)
     const uid = getAuth().currentUser?.uid!
-
+    const userApplications =userApplicationsStore.useState(s=>s)
     React.useEffect(() => {
         uid && update()
         return () => setMounted(false)
@@ -19,12 +19,12 @@ const useUserApplications = () => {
 
   
     const update = () => {
-        setLoading(true)
-        subscripeUserApplications(uid,
-            (v) =>{ 
-                setUserApplications(v) ; 
-            if(mounted){setLoading(false) }
-        return mounted?false:true })
+        // setLoading(true)
+        // subscripeUserApplications(uid,
+        //     (v) =>{ 
+        //         setUserApplications(v) ; 
+        //     if(mounted){setLoading(false) }
+        // return mounted?false:true })
     }
     
 
--- src/hooks/useUserOrders.ts
+++ src/hooks/useUserOrders.ts
@@ -1,34 +1,38 @@
-import { getAuth } from 'firebase/auth';
-import { DocumentData, DocumentSnapshot, QuerySnapshot } from 'firebase/firestore';
-import * as React from 'react';
-import { useState } from 'react';
-import { getOrders, subscripeUserOrders, } from '../providers/firebaseMain';
-
-
+import { getAuth } from "firebase/auth";
+import {
+  DocumentData,
+  DocumentSnapshot,
+  QuerySnapshot,
+} from "firebase/firestore";
+import * as React from "react";
+import { useState } from "react";
+import {
+  getOrders,
+  subscripeUserOrders,
+  userOrdersStore,
+} from "../providers/firebaseMain";
 
 const useUserOrders = () => {
-    const [orders, setOrders] = React.useState<QuerySnapshot<DocumentData>>()
-    const [loading, setLoading] = React.useState<boolean>(true)
-    const [mounted, setMounted] = useState(true)
-    const uid = getAuth().currentUser?.uid!
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const [mounted, setMounted] = useState(true);
+  const uid = getAuth().currentUser?.uid!;
+  const orders = userOrdersStore.useState();
+  React.useEffect(() => {
+    uid && update();
+    return () => setMounted(false);
+  }, []);
 
-    React.useEffect(() => {
-        
-        uid && update()
-        return () => setMounted(false)
-    }, [])
+  const update = () => {
+    // setLoading(true);
+    // subscripeUserOrders(uid, (v) => {
+    //   setOrders(v);
+    //   if (mounted) {
+    //     setLoading(false);
+    //   }
+    //   return mounted ? false : true;
+    // });
+  };
 
-  
-    const update = () => {
-        setLoading(true)
-        subscripeUserOrders(uid,
-            (v) =>{ 
-                setOrders(v) ; 
-            if(mounted){setLoading(false) }
-        return mounted?false:true })
-    }
-    
-
-    return { orders, loading, update };
-}
-export default useUserOrders
+  return { orders, loading, update };
+};
+export default useUserOrders;
