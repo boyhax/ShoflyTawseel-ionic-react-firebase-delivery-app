1bf903f3ca4369dd489cdd5398d55dca71ced792
said alhajri <facegoogl@gmail.com>
2023-04-27 23:28:05 +0400

    improved collection pagination

--- src/components/DriversList.tsx
+++ src/components/DriversList.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 
 import {
   IonButton,
@@ -7,83 +7,31 @@ import {
   IonIcon,
   IonInfiniteScroll,
   IonInfiniteScrollContent,
-  IonLabel,
   IonList,
   IonNote,
   IonRefresher,
   IonRefresherContent,
   IonSegment,
   IonSegmentButton,
+  IonThumbnail,
 } from "@ionic/react";
 import { thumbsUp } from "ionicons/icons";
 import { RefresherEventDetail } from "@ionic/core";
 import OrdersPlaceHolder from "./OrdersPLaceHolder";
-import { useHistory } from "react-router";
 import { mydb } from "../api/firebaseMain";
-import { driverData } from "../types";
-import useMounted from "../hooks/useMounted";
+import { driverData, DriverStatus } from "../types";
 import { TT } from "./utlis/tt";
 import useCollectionPagination from "../hooks/useCollectionPagination";
-import {
-  collection,
-  getFirestore,
-  query,
-  Query,
-  where,
-} from "firebase/firestore";
+import { collection, getFirestore, query, where } from "firebase/firestore";
 
 export default function DriversList(props: any) {
-  // const [list, setList] = useState<driverData[]>([]);
-  const IonRefresherElement = useRef<HTMLIonRefresherElement | any>();
-  const history = useHistory();
-  const [lastDoc, setLastDoc] = useState<any>(null);
-  // const [loading, setLoading] = useState<any>(true);
-  const { mounted } = useMounted();
   const [segment, setSegmt] = useState<"pending" | "active" | "inactive" | any>(
     "pending"
   );
-  const [theQuery, setTheQuery] = useState(
-    query(collection(getFirestore(), "drivers"), where("status", "==", segment))
-  );
 
-  const { documents, getMore, hasMore, loading } = useCollectionPagination(
-    theQuery,
-    10
-  );
-  function Refresh(event: CustomEvent<RefresherEventDetail>) {
-    console.log("Begin async operation");
-    setTheQuery(theQuery);
-    setTimeout(() => {
-      console.log("Async operation has ended");
-      event.detail.complete();
-    }, 2000);
-  }
-  useEffect(() => {
-    setTheQuery(
-      query(
-        collection(getFirestore(), "drivers"),
-        where("status", "==", segment)
-      )
-    );
-  }, [segment]);
 
-  useEffect(() => {
-    console.log("drivers list :>> ", documents);
-  }, [documents]);
+  
 
-  function onEndRefresh(e: any) {
-    !loading && hasMore && getMore();
-
-    setTimeout(() => {
-      e.target.complete();
-    }, 2000);
-  }
-  function hundleApprove(id: string) {
-    console.log("approving driver");
-    mydb.ApproveDriver(id).then((v: any) => {
-      // mounted && setList(list.filter((v: any) => v.id !== id));
-    });
-  }
   return (
     <div>
       <IonSegment onIonChange={(e) => setSegmt(e.detail.value)} value={segment}>
@@ -91,7 +39,44 @@ export default function DriversList(props: any) {
         <IonSegmentButton value="pending">pending</IonSegmentButton>
         <IonSegmentButton value="inactive">inactive</IonSegmentButton>
       </IonSegment>
-      <IonList>
+        {segment === "pending" && <DriversListComponent type={DriverStatus.pending} />}
+        {segment === "active" && <DriversListComponent type={DriverStatus.active} />}
+        {segment === "banned" && <DriversListComponent type={DriverStatus.banned} />}
+
+    </div>
+  );
+}
+
+function DriversListComponent({type=DriverStatus.pending}) {
+  function onEndRefresh(e: any) {
+    !loading && hasMore && loadMore();
+
+    setTimeout(() => {
+      e.target.complete();
+    }, 2000);
+  }
+  const IonRefresherElement = useRef<HTMLIonRefresherElement | any>();
+
+  const [theQuery] = useState( query(
+      collection(getFirestore(), "drivers"),
+      where("status", "==", type)
+    ))
+
+
+  const { hasMore, items, loading, loadMore } = useCollectionPagination(
+    theQuery,
+    { limit: 10 }
+  );
+  function Refresh(event: CustomEvent<RefresherEventDetail>) {
+    console.log("Begin async operation");
+    // setTheQuery(theQuery);
+    setTimeout(() => {
+      console.log("Async operation has ended");
+      event.detail.complete();
+    }, 2000);
+  }
+  return (
+    <IonList>
         <IonRefresher
           ref={IonRefresherElement}
           slot="fixed"
@@ -99,29 +84,12 @@ export default function DriversList(props: any) {
         >
           <IonRefresherContent refreshingText="refreshing..."></IonRefresherContent>
         </IonRefresher>
-
-        {documents &&
-          documents.map((doc:any, i: any) => {
-            let v = doc.data();
-            return (
-              <IonCard key={i}>
-                
-                <IonChip>carNumber {v.carNumber}</IonChip>
-                <IonChip>carType {v.carType}</IonChip>
-
-                <IonChip> carYear{v.carYear}</IonChip>
-                <IonChip>identity {v.identity}</IonChip>
-                <IonNote>status {v.status}</IonNote>
-                <IonButton
-                  onClick={() => {
-                     hundleApprove(doc.id);
-                  }}
-                >
-                  <IonIcon icon={thumbsUp} />
-                </IonButton>
-              </IonCard>
-            );
-          })}
+        {items &&
+        items.map((doc: any, i: any) => {
+          let v = doc.data() as driverData;
+          let id = doc.id;
+          return driverCard(v, id, i);
+        })}
         {loading && <OrdersPlaceHolder></OrdersPlaceHolder>}
         <IonInfiniteScroll
           onIonInfinite={onEndRefresh}
@@ -134,6 +102,45 @@ export default function DriversList(props: any) {
           ></IonInfiniteScrollContent>
         </IonInfiniteScroll>
       </IonList>
-    </div>
+      
+    
   );
+}
+const driverCard = (data: driverData, id: string, i = 1) => {
+  return (
+    <IonCard key={i}>
+      <IonChip>driver_id {data.driver_id}</IonChip>
+      <IonChip>user id {id}</IonChip>
+      <div className={"flex flex-wrap"}>
+        <IonThumbnail className={"w-52 h-52"}>
+          <img src={data.car_card_image} />{" "}
+        </IonThumbnail>
+        <IonThumbnail className={"w-52 h-52"}>
+          <img src={data.car_image} />{" "}
+        </IonThumbnail>
+        <IonThumbnail className={"w-52 h-52"}>
+          <img src={data.driver_id_image} />{" "}
+        </IonThumbnail>
+        <IonThumbnail className={"w-52 h-52"}>
+          <img src={data.driving_license_image} />{" "}
+        </IonThumbnail>
+      </div>
+
+      <IonNote>status {data.status}</IonNote>
+      <IonButton
+        onClick={() => {
+          hundleApprove(data);
+        }}
+      >
+        <IonIcon icon={thumbsUp} />
+      </IonButton>
+    </IonCard>
+  );
+};
+function hundleApprove(doc: driverData) {
+  console.log("approving driver");
+  doc.status === DriverStatus.pending &&
+    mydb.updateDriverData({ status: DriverStatus.active });
+  doc.status === DriverStatus.active &&
+    mydb.updateDriverData({ status: DriverStatus.pending });
 }
--- src/hooks/useCollectionPagination.ts
+++ src/hooks/useCollectionPagination.ts
@@ -1,56 +1,154 @@
-//firestore collection pagination hook
-import { useState, useEffect } from "react";
-import {
-  Query,
-  getDocs,
-  startAfter,
-  query,
-  limit,
-} from "firebase/firestore";
+import { DocumentData, query,limit as _Limit, Query, QueryDocumentSnapshot, QuerySnapshot, onSnapshot } from "firebase/firestore";
+import { useReducer, useEffect } from "react";
 
-export default function useCollectionPagination(
-  theQuery: Query,
-  limitNum: number
-) {
-  const [documents, setDocuments] = useState<any[]>([]);
-  const [lastDoc, setLastDoc] = useState<any>();
-  const [loading, setLoading] = useState<boolean>(false);
-  const [hasMore, setHasMore] = useState<boolean>(true);
+type StateType = {
+  hasMore: boolean;
+  items: DocumentData[];
+  after: QueryDocumentSnapshot | null;
+  lastLoaded: QueryDocumentSnapshot | null;
+  loadingMore: boolean;
+  limit: number;
+  loadingMoreError: null | Error;
+  loading: boolean;
+  loadingError: null | Error;
+};
 
-  useEffect(() => {
-    setDocuments([]);
-    setLastDoc(undefined);
-    setHasMore(true);
-  }, [theQuery]);
+type Action<K, V = void> = V extends void ? { type: K } : { type: K } & V;
 
-  useEffect(() => {
-      setLoading(true);
-      const q = query(theQuery, limit(limitNum));
-      getDocs(q).then((snapshot) => {
-        if (snapshot.docs.length) {
-          setLastDoc(snapshot.docs[snapshot.docs.length - 1]);
-          setDocuments((prev) => [...prev, ...snapshot.docs]);
-        } else {
-          setHasMore(false);
+export type ActionType =
+  | Action<"LOAD-MORE">
+  | Action<
+      "LOADED",
+      {
+        value: QuerySnapshot;
+        limit: number;
+      }
+    >;
+
+const initialState = {
+  hasMore: false,
+  after: null,
+  limit: 0,
+  items: [],
+  lastLoaded: null,
+  loading: true,
+  loadingError: null,
+  loadingMore: false,
+  loadingMoreError: null
+};
+
+function reducer(state: StateType, action: ActionType): StateType {
+  switch (action.type) {
+    case "LOADED": {
+      let items = [...state.items];
+      let isAdding = false;
+
+      action.value.docChanges().forEach(change => {
+        if (change.type === "added") {
+          isAdding = true;
+          addItem(change.doc, items);
+        } else if (change.type === "modified") {
+          updateItem(change.doc, items);
+        } else if (change.type === "removed") {
+          deleteItem(change.doc, items);
         }
-        setLoading(false);
       });
-    
-  }, [theQuery,limitNum]);
-  function getMore() {
-    if (hasMore) {
-      setLoading(true);
-      const q = query(theQuery, startAfter(lastDoc), limit(limitNum));
-      getDocs(q).then((snapshot) => {
-        if (snapshot.docs.length) {
-          setLastDoc(snapshot.docs[snapshot.docs.length - 1]);
-          setDocuments((prev) => [...prev, ...snapshot.docs]);
-        } else {
-          setHasMore(false);
-        }
-        setLoading(false);
-      });
+
+      const nextLimit = items.length + action.limit;
+
+      let end = items.length < action.limit || nextLimit === state.limit;
+
+      return {
+        ...state,
+        hasMore: isAdding ? !end : state.hasMore,
+        limit: nextLimit,
+        loading: false,
+        loadingError: null,
+        lastLoaded: action.value.docs[action.value.docs.length - 1],
+        loadingMore: false,
+        items
+      };
+    }
+
+    case "LOAD-MORE": {
+      return {
+        ...state,
+        loadingMore: true,
+        after: state.lastLoaded
+      };
     }
   }
-  return { documents, loading, hasMore, getMore };
+}
+
+function findIndexOfDocument(
+  doc: QueryDocumentSnapshot,
+  items: DocumentData[]
+) {
+  return items.findIndex(item => {
+    return item.id === doc.id;
+  });
+}
+
+function updateItem(
+  doc: QueryDocumentSnapshot,
+  items: DocumentData[]
+) {
+  const i = findIndexOfDocument(doc, items);
+  items[i] = doc;
+}
+
+function deleteItem(
+  doc: QueryDocumentSnapshot,
+  items: DocumentData[]
+) {
+  const i = findIndexOfDocument(doc, items);
+  items.splice(i, 1);
+}
+
+function addItem(
+  doc: QueryDocumentSnapshot,
+  items: DocumentData[]
+) {
+  const i = findIndexOfDocument(doc, items);
+  if (i === -1) {
+    items.push(doc);
+  }
+}
+
+interface PaginationOptions {
+  // how many documents should we fetch at a time?
+  limit?: number;
+}
+
+export default function useCollectionPagination(
+  _query: Query,
+  { limit = 25 }: PaginationOptions = {}
+) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // when "after" changes, we update our query
+  useEffect(() => {
+    let fn = query(_query,_Limit(state.limit || limit)) ;
+
+    let unsubscribe = onSnapshot(fn,(snap:any) => {
+      dispatch({ type: "LOADED", value: snap, limit });
+    });
+
+    return () => unsubscribe();
+  }, [state.after]);
+
+  // trigger firebase to load more
+  function loadMore() {
+    dispatch({ type: "LOAD-MORE" });
+  }
+
+  return {
+    loadingMore: state.loadingMore,
+    loadingError: state.loadingError,
+    loadingMoreError: state.loadingMoreError,
+    loading: state.loading,
+    hasMore: state.hasMore,
+    items: state.items,
+    loadMore
+  };
 }
